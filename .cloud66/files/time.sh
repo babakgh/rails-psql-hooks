b9955b6e1c47237dd1ab9799ac43ae2897e4cd7d
Philip Källberg <philip@cloud66.com>
2014-03-14 15:07:13 +0000

    Update time.sh

--- .cloud66/files/time.sh
+++ .cloud66/files/time.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-FILE=/tmp/time_done
-
-if [ -f $FILE ]
-then
-	echo "File $FILE exists..."
-else
-    cd /etc
-    rm localtime
-    ln -s /usr/share/zoneinfo/Europe/Berlin localtime
-    ntpd -q
-    touch /tmp/time_done
-fi
+cd /etc
+rm localtime
+ln -s /usr/share/zoneinfo/Europe/Berlin localtime
+ntpd -q
+touch /tmp/time_done
